3e563570b8b9c1db3e4ec494478b22af02defde4
tonybailey <tony@elementum.com>
2014-02-25 09:57:54 -0800

    -

--- js/quote.js
+++ js/quote.js
@@ -1,4 +1,4 @@
-var mess = new Array('code0','code1','code2');
+var mess = new Array('Design is the silent ambassador<br/>of your brand<br/><small>Paul Rand</small>','code1','code2');
 var max = mess.length;
 var num = Math.floor((Math.random() * max));
 document.writeln(mess[num]);
