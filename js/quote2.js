b89ac84be26501e46cc4e40e1c2f147e70bcde8f
tonybailey <tony@elementum.com>
2014-03-03 11:14:19 -0800

    -

---
+++ js/quote2.js
@@ -0,0 +1,5 @@
+var mess = new Array('You can&#8217;t marry nine women<br/>and have a baby in a month<br/><small>DOMINIC VIA DANA</small>','You can only make so many mistakes<br/>before you run out of mistakes.<br/><small>MIGUEL</small>','You can&#8217;t be half pregnant.<br/><small>NATHAN</small>','Grrr, WOOF!<br/><small>PANDA</small>');
+var max = mess.length;
+var num = Math.floor((Math.random() * max));
+document.writeln(mess[num]);
+
