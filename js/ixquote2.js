a620869753497ec86242caf5904564e624bdda23
tonybailey <tony@elementum.com>
2014-03-17 10:13:09 -0700

    -

---
+++ js/ixquote2.js
@@ -0,0 +1,5 @@
+var mess = new Array('I&#8217;M TOO BUSY WRITING HISTORY TO READ IT.<br/><small>CREATIVE DIRECTOR or KANYE WEST</small>','THERE&#8217;S A THOUSAND YOU&#8217;S, THERE&#8217;S ONLY ONE OF ME.<br/><small>CREATIVE DIRECTOR or KANYE WEST</small>','THERE&#8217;S NOTHING MORE MANLY THAN SHAKESPEARE.<br/><small>CREATIVE DIRECTOR or KANYE WEST</small>','I THINK WHAT YOU&#8217;VE DONE IS GOOD, BUT I ONLY RESPOND TO GREATNESS.<br/><small>CREATIVE DIRECTOR or KANYE WEST</small>');
+var max = mess.length;
+var num = Math.floor((Math.random() * max));
+document.writeln(mess[num]);
+
